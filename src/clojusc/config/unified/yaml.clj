687cfce3df62f6e4dc4261d95d94df9f69bd69e6
Duncan McGreggor <oubiwann@gmail.com>
2019-04-04 12:47:10 -0500

    Added support for reading YAML from string.

--- src/clojusc/config/unified/yaml.clj
+++ src/clojusc/config/unified/yaml.clj
@@ -3,7 +3,8 @@
    [clojure.string :as string]
    [clojusc.config.unified.common :as common]
    [clojusc.config.unified.util :as util]
-   [taoensso.timbre :as log]))
+   [taoensso.timbre :as log]
+   [yaml.core :as yaml]))
 
 (def config-file "config/clojusc-unified-config/config.yml")
 
@@ -20,3 +21,11 @@
     (util/deep-merge (base-data filename)
                      (common/props-data)
                      (common/env-data))))
+
+(defn data-str
+  [yml-str]
+  (util/deep-merge (yaml/parse-string yml-str)
+                   (common/props-data)
+                   (common/env-data)))
+
+(def data-file #'data)
